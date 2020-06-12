ace9db1de1fc55c1bc09761f8d0c366b057f634b
Piotr262 <66378116+Piotr262@users.noreply.github.com>
2020-06-12 21:08:34 +0200

    server.R for Shiny

---
+++ main_file/server.R
@@ -0,0 +1,53 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+data <- readRDS("beijing_super_clean.rds")
+
+
+# Define server logic
+shinyServer(function(input, output) {
+  
+  top_10_followers <- function(){
+    data %>% 
+      filter(district == input$district) %>%
+      filter(renovationCondition==input$renovationCondition) %>%
+      filter(building_age == input$building_age) %>%
+      filter(elevator==input$elevator) %>%
+      filter(subway==input$subway) %>%
+      filter(totalPrice<=input$totalPrice[2]) %>%
+      filter(totalPrice>=input$totalPrice[1]) %>%
+      filter(square<=input$square[2]) %>%
+      filter(square>=input$square[1]) %>%
+      filter(tradeTime<=input$tradeTime[2]) %>%
+      filter(tradeTime>=input$tradeTime[1])
+  }
+  
+  price_time_trends <- function(){
+    data %>% 
+      filter(district == input$district) %>% 
+      filter(renovationCondition==input$renovationCondition) %>%
+      filter(building_age == input$building_age) %>%
+      filter(elevator==input$elevator) %>%
+      filter(subway==input$subway) %>%
+      filter(totalPrice<=input$totalPrice[2]) %>%
+      filter(totalPrice>=input$totalPrice[1]) %>%
+      filter(square<=input$square[2]) %>%
+      filter(square>=input$square[1]) %>%
+      filter(tradeTime<=input$tradeTime[2]) %>%
+      filter(tradeTime>=input$tradeTime[1]) %>%
+      ggplot(aes(x = tradeTime, y = price)) +
+      geom_col()+
+      ggtitle("The housing price trend among each district")
+  }
+  
+  
+  # MODIFY CODE BELOW: Render a DT output named "table_top_10_names"
+  output$table_top_10_followers <- DT::renderDT({
+    DT::datatable(top_10_followers())
+  })
+  output$price_according_to_time<-plotly::renderPlotly({
+    price_time_trends()
+  })
+  
+})
